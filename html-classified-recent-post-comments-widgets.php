0251528a00f7ab79f09a96dc13285424fd798ec4
zaantar <zaantar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-22 05:21:43 +0000

    1.0
    git-svn-id: https://plugins.svn.wordpress.org/html-classified-recent-posts-comments-widgets/trunk@588722 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ html-classified-recent-post-comments-widgets.php
@@ -0,0 +1,227 @@
+<?php
+/*
+Plugin Name: HTML Classified Recent Posts & Comments Widgets
+Description: Default WordPress widgets with "class" attributes identifying posts added to links.
+Author: Zaantar
+Version: 1.0
+Author URI: http://zaantar.eu
+Donate Link: http://zaantar.eu/financni-prispevek
+Plugin URI: http://wordpress.org/extend/plugins/classified-default-widgets/
+License: GPLv2 or later
+*/
+
+add_action( 'widgets_init', 'crpw_init' );
+
+
+function crpw_init() {
+	register_widget( "Classified_Recent_Posts" );
+	register_widget( "Classified_Recent_Comments" );
+}
+
+
+class Classified_Recent_Posts extends WP_Widget  {
+
+	function __construct() {
+		$widget_ops = array('classname' => 'widget_classified_recent_entries', 'description' => __( "The most recent posts on your site") );
+		parent::__construct('classified-recent-posts', __('Classified Recent Posts'), $widget_ops);
+		$this->alt_option_name = 'widget_classified_recent_entries';
+
+		add_action( 'save_post', array(&$this, 'flush_widget_cache') );
+		add_action( 'deleted_post', array(&$this, 'flush_widget_cache') );
+		add_action( 'switch_theme', array(&$this, 'flush_widget_cache') );
+	}
+
+	function widget($args, $instance) {
+		$cache = wp_cache_get('widget_recent_posts', 'widget');
+
+		if ( !is_array($cache) )
+			$cache = array();
+
+		if ( ! isset( $args['widget_id'] ) )
+			$args['widget_id'] = $this->id;
+
+		if ( isset( $cache[ $args['widget_id'] ] ) ) {
+			echo $cache[ $args['widget_id'] ];
+			return;
+		}
+
+		ob_start();
+		extract($args);
+
+		$title = apply_filters('widget_title', empty($instance['title']) ? __('Recent Posts') : $instance['title'], $instance, $this->id_base);
+		if ( empty( $instance['number'] ) || ! $number = absint( $instance['number'] ) )
+ 			$number = 10;
+
+		$r = new WP_Query( apply_filters( 'widget_posts_args', array( 'posts_per_page' => $number, 'no_found_rows' => true, 'post_status' => 'publish', 'ignore_sticky_posts' => true ) ) );
+		if ($r->have_posts()) :
+?>
+		<?php echo $before_widget; ?>
+		<?php if ( $title ) echo $before_title . $title . $after_title; ?>
+		<ul>
+		<?php 
+			while ($r->have_posts()) {
+				$r->the_post(); 
+				$classes = get_post_class();
+				foreach( $classes as $key => $class ) {
+					$classes[$key] = "crpw-{$class}";
+				}
+				$classes = implode( " ", $classes );
+				?>
+				<li>
+					<a 
+						href="<?php the_permalink() ?>" 
+						title="<?php echo esc_attr(get_the_title() ? get_the_title() : get_the_ID()); ?>"
+						class="<?php echo $classes; ?>"
+					  ><?php if ( get_the_title() ) the_title(); else the_ID(); ?>
+					</a>
+				</li>
+				<?php 
+			} 
+		?>
+		</ul>
+		<?php echo $after_widget; ?>
+<?php
+		// Reset the global $the_post as this query will have stomped on it
+		wp_reset_postdata();
+
+		endif;
+
+		$cache[$args['widget_id']] = ob_get_flush();
+		wp_cache_set('widget_recent_posts', $cache, 'widget');
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['number'] = (int) $new_instance['number'];
+		$this->flush_widget_cache();
+
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
+		if ( isset($alloptions['widget_classified_recent_entries']) )
+			delete_option('widget_classified_recent_entries');
+
+		return $instance;
+	}
+
+	function flush_widget_cache() {
+		wp_cache_delete('widget_recent_posts', 'widget');
+	}
+
+	function form( $instance ) {
+		$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
+		$number = isset($instance['number']) ? absint($instance['number']) : 5;
+?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+
+		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of posts to show:'); ?></label>
+		<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+<?php
+	}
+
+}
+
+
+class Classified_Recent_Comments extends WP_Widget {
+
+	function __construct() {
+		$widget_ops = array('classname' => 'widget_classified_recent_comments', 'description' => __( 'The most recent comments' ) );
+		parent::__construct('classified-recent-comments', __('Classified Recent Comments'), $widget_ops);
+		$this->alt_option_name = 'widget_classified_recent_comments';
+
+		if ( is_active_widget(false, false, $this->id_base) )
+			add_action( 'wp_head', array(&$this, 'recent_comments_style') );
+
+		add_action( 'comment_post', array(&$this, 'flush_widget_cache') );
+		add_action( 'transition_comment_status', array(&$this, 'flush_widget_cache') );
+	}
+
+	function recent_comments_style() {
+		if ( ! current_theme_supports( 'widgets' ) // Temp hack #14876
+			|| ! apply_filters( 'show_recent_comments_widget_style', true, $this->id_base ) )
+			return;
+		?>
+	<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+<?php
+	}
+
+	function flush_widget_cache() {
+		wp_cache_delete('widget_classified_recent_comments', 'widget');
+	}
+
+	function widget( $args, $instance ) {
+		global $comments, $comment;
+
+		$cache = wp_cache_get('widget_classified_recent_comments', 'widget');
+
+		if ( ! is_array( $cache ) )
+			$cache = array();
+
+		if ( ! isset( $args['widget_id'] ) )
+			$args['widget_id'] = $this->id;
+
+		if ( isset( $cache[ $args['widget_id'] ] ) ) {
+			echo $cache[ $args['widget_id'] ];
+			return;
+		}
+
+ 		extract($args, EXTR_SKIP);
+ 		$output = '';
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Recent Comments' ) : $instance['title'], $instance, $this->id_base );
+
+		if ( empty( $instance['number'] ) || ! $number = absint( $instance['number'] ) )
+ 			$number = 5;
+
+		$comments = get_comments( apply_filters( 'widget_comments_args', array( 'number' => $number, 'status' => 'approve', 'post_status' => 'publish' ) ) );
+		$output .= $before_widget;
+		if ( $title )
+			$output .= $before_title . $title . $after_title;
+
+		$output .= '<ul id="recentcomments">';
+		if ( $comments ) {
+			foreach ( (array) $comments as $comment) {
+				$classes = get_post_class( "", $comment->comment_post_ID );
+				foreach( $classes as $key => $class ) {
+					$classes[$key] = "crcw-{$class}";
+				}
+				$classes = implode( " ", $classes );
+				$output .=  '<li class="recentcomments ' . $classes . '">' . /* translators: comments widget: 1: comment author, 2: post link */ sprintf(_x('%1$s on %2$s', 'widgets'), get_comment_author_link(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '" class="' . $classes . '">' . get_the_title($comment->comment_post_ID) . '</a>') . '</li>';
+			}
+ 		}
+		$output .= '</ul>';
+		$output .= $after_widget;
+
+		echo $output;
+		$cache[$args['widget_id']] = $output;
+		wp_cache_set('widget_classified_recent_comments', $cache, 'widget');
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['number'] = absint( $new_instance['number'] );
+		$this->flush_widget_cache();
+
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
+		if ( isset($alloptions['widget_classified_recent_comments']) )
+			delete_option('widget_classified_recent_comments');
+
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
+		$number = isset($instance['number']) ? absint($instance['number']) : 5;
+?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+
+		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of comments to show:'); ?></label>
+		<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+<?php
+	}
+}
+
+
+
+?>
